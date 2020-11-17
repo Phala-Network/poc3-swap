c0e95687127e39fff2fc7205297226c4965a61cd
luswar <luswar@163.com>
2020-11-17 16:25:53 +0800

    Fix burnAmountNote

--- src/msg.js
+++ src/msg.js
@@ -2,7 +2,7 @@ const burnWarning = "You are going to BURN 0.1PHA tokens on Ethereum. Once it's 
     "\nWarning: tPHA is worth ZERO and will NOT be converted to any real tradable token in the future. tPHA is only for testing purpose. You CANNOT change tPHA back to ERC20 PHA as well.\n" +
     "\nPhala added this design to prevent spam miners and fake competition in Miner Race Ranking.";
 
-const burnAmountNote = "The amount is restricted to 0.1 PHA on the UI to avoid accidental asset loss. Ask in our Discord if you want swap more."
+const burnAmountNote = "The amount is restricted to 0.1 PHA to avoid accidental asset loss. Ask in our Discord if you want swap more."
 const burnTxLinkPrefix = "You can find your burning transaction id on ";
 const burnTxLinkSuffix = ". You can claim the swapped tPHA only after the transaction is confirmed on the Ethereum blockchain.";
 const claimTxLinkPrefix = "Successfully claimed. You can check you balance at ";
